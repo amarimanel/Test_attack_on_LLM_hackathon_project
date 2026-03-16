f57daea9a55a418236035ac2f24435fba4a97009
amarimanel <amaarimaanel@gmail.com>
2026-03-16 20:26:44 +0100

    Ajout des routes URLs et de la structure de la page de test

--- webapp/security/views.py
+++ webapp/security/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'security/home.html')
+
+def test_page(request):
+    # C'est ici qu'on fera l'appel aux modèles plus tard
+    return render(request, 'security/test.html')
+
+def stats_page(request):
+    return render(request, 'security/stats.html')
